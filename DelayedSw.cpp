ca0e700760be79b74a8206515e16db7c90dc9a89
Dario Desktop <dar8900@gmail.com>
2020-08-30 19:29:16 +0200

    -Inserite le prime funzioni per la gestione del pulsante, del rele e del timer di spegnimento

--- DelayedSw.cpp
+++ DelayedSw.cpp
@@ -1,2 +1,102 @@
 #include "DelayedSwitch.h"
 
+Chrono TimerSwitch(Chrono::SECONDS);
+
+void RELE_CTRL::setup()
+{
+    pinMode(RELE_PIN, OUTPUT);
+}
+
+void RELE_CTRL::switchRele(bool Status)
+{
+    if(oldStatus != Status)
+    {
+        if(Status == ON)
+        {
+            digitalWrite(RELE_PIN, HIGH);
+        }
+        else
+        {
+            digitalWrite(RELE_PIN, LOW);
+        }
+        oldStatus = Status;        
+    }
+}
+
+
+void TIMER_CTRL::setup(uint32_t &Timer)
+{
+    Timer = 0;
+}
+
+void TIMER_CTRL::updateTimer(uint32_t &Timer, bool &Status)
+{
+    if(Status == ON)
+    {
+        if(TimerSwitch.hasPassed(60, true))
+        {
+            if(Timer > 0)
+            {
+                Timer--;
+            }
+        }
+        if(Timer == 0)
+        {
+            Status = OFF;
+            TimerSwitch.restart();
+        }
+    }
+}
+
+void BUTTON_CTRL::setup()
+{
+    pinMode(BUTTON_PIN, INPUT);
+}
+
+void BUTTON_CTRL::checkButton(uint32_t &Timer, bool &Status)
+{
+    uint32_t Elapsed = 0;
+    bool ButtonStatus = false;
+    buttonPressed = NO_PRESS;
+    ButtonStatus = (bool)digitalRead(BUTTON_PIN);
+    if(ButtonStatus)
+    {
+        Elapsed = millis();
+        while(ButtonStatus)
+        {
+            ButtonStatus = (bool)digitalRead(BUTTON_PIN);
+            if(millis() - Elapsed > 1000)
+            {
+                buttonPressed = LONG_PRESSED;
+                break;
+            }
+            else
+            {
+                buttonPressed = PRESSED;
+            }
+            
+        }
+        if(buttonPressed == LONG_PRESSED)
+        {
+            Timer = 0;
+            Status = OFF;
+        }
+        else if(buttonPressed == PRESSED)
+        {
+            Timer++;
+            Status = ON;
+        }
+        delay(25);
+    }
+}
+
+
+void CURRENT_SENSOR::setup()
+{
+
+}
+
+void CURRENT_SENSOR::calcCurrent(float &Current)
+{
+    
+}
--- DelayedSwitch.h
+++ DelayedSwitch.h
@@ -4,40 +4,75 @@
 #include <Arduino.h>
 #include <SSD1306_minimal.h>
 #include <Chrono.h>
+#include <EEPROM.h>
+#include <stdint.h>
+
+#define ON      true
+#define OFF     false
+
+
+#define RELE_PIN            1
+#define BUTTON_PIN          3
+#define CURRENT_SENSOR      A2
 
 class RELE_CTRL
 {
+    private:
+        bool oldStatus;
     public:
-        setup();
+        void setup();
+        void switchRele(bool Status);
 
+};
 
+class TIMER_CTRL
+{
+    public:
+        void setup(uint32_t &Timer);
+        void updateTimer(uint32_t &Timer, bool &Status);
 };
 
 class BUTTON_CTRL
 {
+    private:
+        enum
+        {
+            PRESSED = 0,
+            LONG_PRESSED,
+            NO_PRESS
+        };
+        uint8_t buttonPressed = NO_PRESS;
+        uint8_t oldButtonStatus = NO_PRESS;
+
     public:
-        setup();
+        void setup();
+        void checkButton(uint32_t &Timer, bool &Status);
 };
 
 class OLED_CTRL
 {
     public:
-        setup();
+        void setup();
+        void showAllInfo(uint32_t Timer, bool Status, float Current);
 };
 
 class CURRENT_SENSOR
 {
     public:
-        setup();
+        void setup();
+        void calcCurrent(float &Current);
 };
 
 class DELAYED_SWITCH
 {
     private:
         uint32_t switchTimer;
+        bool status;
+        float current;
         
     public:
-        setup();
+        void setup();
+        void runDelayedSwitch();
 };
 
 #endif
